5789f6980c7c38a40c00b5ffec8a148a4dcd4b1f
Winter3un <511683586@qq.com>
2016-06-24 10:09:59 +0800

    2016/6/24

--- pwn5/calc.py
+++ pwn5/calc.py
@@ -1,5 +1,5 @@
-end = 'DEADBEEF'
-# start = 5381
+end = '2029bc3c'
+
 import sys   
 sys.setrecursionlimit(1000000)
 
@@ -8,21 +8,17 @@ def get(end):
 		return True
 	if end <1234:
 		return False
-	for x in range(1,0xff+1):
-		if (end - 12) % 34 ==0:
-			flag = get((end - 12) / 34 -x)
-			if flag:
-				print hex(end),hex(x)
-				return True
-			else:
-				return False
+	if (end - 12) % 34 !=0:
+		return False
+	for x in xrange(1,0xff+1):
+		flag = get((end - 12) / 34 -x)
+		if flag:
+			print hex(end),hex(x)
+			return True
+		else:
+			continue
 				
-for x in range(0,0xffffff):
+for x in xrange(0,0xfffffff):
 	tmp  = int(hex(x).replace('0x','')+end,16)
-	# if (tmp - 0xa) % 33 ==0:
-	# 	if get((tmp - 0x0a) / 33):
-	# 		print hex(tmp),hex(0x0a)
-	# 		print 'end'
-	# 		exit(0)
-	if get(tem,16):
+	if get(tmp):
 		print 'end'
